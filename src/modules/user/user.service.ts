eefea79396fef466181a85901c8e538cb788b36b
Xmayora <xmayora@wingsoft.com>
2024-04-12 09:39:22 -0400

    adding create user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,19 @@
+import AppDataSource from "../../data.source";
+import User from "./user.entity";
+
 export default class userService {
-  GET() {
-    return "hola";
+  async GET() {
+    const users = await User.find()
+    console.log(users)
+    return []
   }
-  POST(body: any) {
-    return body
+  async POST(body: any) {
+    const user = new User()    
+    user.firstName = body.firstName
+    user.lastName = body.lastName
+    user.email = body.email
+    user.password = body.password
+    console.log( await user.save())
+    return user
   }
 }
