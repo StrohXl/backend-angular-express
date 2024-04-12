64a5f20c2b683ecb63bd5556eb587a2da374836f
Xmayora <xmayora@wingsoft.com>
2024-04-12 09:37:58 -0400

    adding console.log(localhost) in initializie

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,20 @@ import express, { Request, Response, Express, json } from "express";
 import cors from "cors";
 import routerApp from "./app.routes";
 import AppDataSource from "./data.source";
-AppDataSource.initialize().then(() => {
-  console.log("Data Source has been initialized!")
-}).catch((error) => {
-  console.error(error)
-})
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+    console.log(`Servidor escuchando en el puerto http://localhost:${port}`);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
 const app: Express = express();
 const port: number = 8000;
 app.use(cors());
-app.use(json())
+app.use(express.json());
 routerApp(app);
 
 app.get("/", (req: Request, res: Response) => {
