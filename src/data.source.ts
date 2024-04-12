d6e64b84fe6147fdd1df8c34aad2794a398d8771
StrohXl <xaviermayora20@gmail.com>
2024-04-12 16:14:13 -0400

    adding env

--- src/data.source.ts
+++ src/data.source.ts
@@ -1,13 +1,14 @@
 import { DataSource } from "typeorm";
 import User from "./modules/user/user.entity";
+import 'dotenv/config'
 
 const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "spd00ankama",
-    database: "postgres",
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: true,
     entities: [User],
